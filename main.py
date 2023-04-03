02794f5cc8c0e080a90bbcd7dd847462a2f660b2
ZaVengefulOne <shalomeenko53@gmail.com>
2023-04-03 21:11:49 +0300

    Started our project

--- main.py
+++ main.py
@@ -18,7 +18,7 @@
 #        await msg.answer('Привет!')
 #    else:
 #        await msg.answer('Не понимаю, что это значит.')
-#
+# test
 # if __name__ == '__main__':
 #     executor.start_polling(dp)
 
