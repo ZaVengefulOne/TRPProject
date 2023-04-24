592237db6d8237678bdfe41192e38d2513dafec2
ZaVengefulOne <shalomeenko53@gmail.com>
2023-04-24 21:17:00 +0300

    Added advanced parcing

--- Parser.py
+++ Parser.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Получаем HTML-код страницы с помощью метода get(url)
 url = "https://eda.ru/recepty"
-response = requests.get(url) # Получаем HTML-код страницы с помощью метода get(url)
+response = requests.get(url)
 
 if response.status_code == 200:
     html_text = response.text
@@ -15,6 +16,39 @@ else:
 soup = BeautifulSoup(html_text, "html.parser")
 recipes_list = soup.find_all(class_='emotion-1eugp2w')
 hrefs = soup.find_all(class_='emotion-18hxz5k', href=True)
+
 for i in range(len(recipes_list)):
-    print(recipes_list[i].text)
-    print(hrefs[i]['href'])
+    print(i+1,")", recipes_list[i].text)
+#    print("\t(", hrefs[i]['href'], ")")
+
+while True:
+    choice = int(input("Введите номер рецепта (от 1 до {}): ".format(len(recipes_list))))
+
+    if choice < 1 or choice > len(recipes_list):
+        print("Неверный ввод. Попробуйте еще раз.")
+
+    recipe_url = "https://eda.ru" + hrefs[choice - 1]['href']
+    recipe_response = requests.get(recipe_url)
+
+    if recipe_response.status_code == 200:
+        recipe_html_text = recipe_response.text
+    else:
+        print('Ошибка:', recipe_response.status_code)
+        recipe_html_text = 0
+
+    recipe_soup = BeautifulSoup(recipe_html_text, "html.parser")
+    recipe_title = recipes_list[choice-1].text
+    recipe_ingredients = recipe_soup.find_all(class_='emotion-mdupit')
+    quantity_recipe_ingredients = recipe_soup.find_all(class_='emotion-bsdd3p')
+    recipe_steps = recipe_soup.find_all(class_='emotion-wdt5in')
+
+    print("Рецепт \"{}\"".format(recipe_title))
+    print("Ингредиенты:")
+    for i in range(len(recipe_ingredients)):
+        ingredient = recipe_ingredients[i].text.strip()
+        quantity = quantity_recipe_ingredients[i].text.strip()
+        print("- {} ({})".format(ingredient, quantity))
+    print("Пошаговый рецепт:")
+    for i, step in enumerate(recipe_steps):
+        print("\t{}. {}".format(i + 1, step.text.strip()))
+    break
--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+import Parser
 
 token = "6077834064:AAEi70eoSxqqIu9YC3eiuQKB2jM1XjP9sMI"
 bot = Bot(token=token)
 dp = Dispatcher(bot)
+url = "https://eda.ru/recepty"
 
 
-
-
-
-@dp.message_handler(commands=['start' , 'help'])
+@dp.message_handler(commands=['start', 'help'])
 async def send_welcome(msg: types.message):
     await msg.reply_to_message(f'Привет, я - бот помощник повара. Приятно познакомиться, {msg.from_user.full_name}')
 
 
 @dp.message_handler(content_types=['text'])
 async def get_text_messages(msg: types.Message):
-   if msg.text.lower() == 'привет':
-       await msg.answer('Привет!')
-   else:
-       await msg.answer('Не понимаю, что это значит.')
+    if msg.text.lower() == 'привет':
+        await msg.answer('Привет!')
+    else:
+        await msg.answer('Не понимаю, что это значит.')
+
+
+def documentation():
+    i = 0
+    for methods in dir(Parser):
+        print(dir(Parser)[i], "--", eval("Parser." + methods + ".__doc__"))
+        i += 1
+
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
-
+    # executor.start_polling(dp)
+    documentation()
