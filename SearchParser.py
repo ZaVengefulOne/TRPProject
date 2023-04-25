31518ea0a3cfdb80aeafc1b7312f26fb69ff1d54
ZaVengefulOne <shalomeenko53@gmail.com>
2023-04-25 13:41:44 +0300

    Added additional parcing for search function and random recipe

--- SearchParser.py
+++ SearchParser.py
@@ -1,16 +1,20 @@
+import random
+
 import requests
 from bs4 import BeautifulSoup
 
 # Получаем HTML-код страницы с помощью метода get(url)
-url = "https://eda.ru/recepty"
+c = str(random.randint(1, 700))
+url = "https://eda.ru/recepty?page=" + c
 response = requests.get(url)
 
-if response.status_code == 200:
-    html_text = response.text
-    # print(html_text)
-else:
-    print('Ошибка:', response.status_code)
-    html_text = 0
+html_text = response.text
+# if response.status_code == 200:
+#
+#     # print(html_text)
+# else:
+#     print('Ошибка:', response.status_code)
+#     html_text = 0
 
 # Парсинг HTML-кода и извлечение списка рецептов
 soup = BeautifulSoup(html_text, "html.parser")
@@ -18,7 +22,7 @@ recipes_list = soup.find_all(class_='emotion-1eugp2w')
 hrefs = soup.find_all(class_='emotion-18hxz5k', href=True)
 
 for i in range(len(recipes_list)):
-    print(i + 1, ")", recipes_list[i].text)
+    print(i+1,")", recipes_list[i].text)
 #    print("\t(", hrefs[i]['href'], ")")
 
 while True:
@@ -29,20 +33,14 @@ while True:
 
     recipe_url = "https://eda.ru" + hrefs[choice - 1]['href']
     recipe_response = requests.get(recipe_url)
-
-    if recipe_response.status_code == 200:
-        recipe_html_text = recipe_response.text
-    else:
-        print('Ошибка:', recipe_response.status_code)
-        recipe_html_text = 0
-
+    recipe_html_text = recipe_response.text
     recipe_soup = BeautifulSoup(recipe_html_text, "html.parser")
-    recipe_title = recipes_list[choice - 1].text
+    recipe_title = recipes_list[choice-1].text
     recipe_ingredients = recipe_soup.find_all(class_='emotion-mdupit')
     quantity_recipe_ingredients = recipe_soup.find_all(class_='emotion-bsdd3p')
     recipe_steps = recipe_soup.find_all(class_='emotion-wdt5in')
 
-    print("Рецепт \"{}\"".format(recipe_title))
+    print(recipe_title)
     print("Ингредиенты:")
     for i in range(len(recipe_ingredients)):
         ingredient = recipe_ingredients[i].text.strip()
