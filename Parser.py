6d68df0c67efc5447d1b3c7e764fdf2decf1f303
ZaVengefulOne <shalomeenko53@gmail.com>
2023-05-23 10:45:22 +0300

    Implemented recipe search for telegram

--- Parser.py
+++ Parser.py
@@ -1,5 +1,125 @@
+import random
+
 import requests
 from bs4 import BeautifulSoup
+
+
+def get_recipe(search):
+    # Получаем HTML-код страницы с помощью метода get(url)
+    url = "https://eda.ru/recipesearch?onlyEdaChecked=true&q=" + search
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        html_text = response.text
+    else:
+        return 'Ошибка при получении рецепта'
+
+    # Парсинг HTML-кода и извлечение списка рецептов
+    soup = BeautifulSoup(html_text, "html.parser")
+    recipes_list = soup.find_all(class_='horizontal-tile__item-title item-title')
+    hrefs = soup.find_all(class_='breadcrumbs', href=True)
+    recipes = []
+    link = []
+    for i in range(len(recipes_list)):
+        recipe = ''
+        letter_flag = False
+        for j in range(len(recipes_list[i].text)):
+            if not letter_flag and recipes_list[i].text[j].isalpha():
+                letter_flag = True
+            if letter_flag:
+                if recipes_list[i].text[j].isalpha() or recipes_list[i].text[j] == ' ' or recipes_list[i].text[j] == '\u00A0':
+                    recipe += recipes_list[i].text[j]
+        recipes.append(recipe)
+
+        recipe_link = 'https://eda.ru'
+        link_start = str(recipes_list[i]).find('href=') + 6
+        link_iterator = link_start
+        while True:
+            if str(recipes_list[i])[link_iterator] == '"':
+                break
+            recipe_link += str(recipes_list[i])[link_iterator]
+            link_iterator += 1
+        link.append(recipe_link)
+
+    # Поиск по запросу
+    choice = 1  # Выбираем первый рецепт из списка
+
+    recipe_url = link[choice - 1]
+    recipe_response = requests.get(recipe_url)
+    recipe_html_text = recipe_response.text
+    recipe_soup = BeautifulSoup(recipe_html_text, "html.parser")
+    recipe_title = recipes[choice - 1]
+    recipe_ingredients = recipe_soup.find_all(class_='emotion-mdupit')
+    quantity_recipe_ingredients = recipe_soup.find_all(class_='emotion-bsdd3p')
+    recipe_steps = recipe_soup.find_all(class_='emotion-wdt5in')
+
+    # Формируем текст рецепта
+    recipe_text = f'Рецепт для "{recipe_title}":\n\n'
+    recipe_text += 'Ингредиенты:\n'
+    for ingredient in recipe_ingredients:
+        recipe_text += f'- {ingredient.text.strip()}\n'
+    recipe_text += '\nПриготовление:\n'
+    for i, step in enumerate(recipe_steps):
+        recipe_text += f'{i+1}. {step.text.strip()}\n'
+
+    return recipe_text
+
+def random_recipe():
+    # Получаем HTML-код страницы с помощью метода get(url)
+    url = "https://eda.ru/recipesearch?onlyEdaChecked=true"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        html_text = response.text
+    else:
+        return 'Ошибка при получении рецепта'
+    # Парсинг HTML-кода и извлечение списка рецептов
+    soup = BeautifulSoup(html_text, "html.parser")
+    recipes_list = soup.find_all(class_='horizontal-tile__item-title item-title')
+    hrefs = soup.find_all(class_='breadcrumbs', href=True)
+    recipes = []
+    link = []
+    for i in range(len(recipes_list)):
+        recipe = ''
+        letter_flag = False
+        for j in range(len(recipes_list[i].text)):
+            if not letter_flag and recipes_list[i].text[j].isalpha():
+                letter_flag = True
+            if letter_flag:
+                if recipes_list[i].text[j].isalpha() or recipes_list[i].text[j] == ' ' or recipes_list[i].text[j] == '\u00A0':
+                    recipe += recipes_list[i].text[j]
+        recipes.append(recipe)
+
+        recipe_link = 'https://eda.ru'
+        link_start = str(recipes_list[i]).find('href=') + 6
+        link_iterator = link_start
+        while True:
+            if str(recipes_list[i])[link_iterator] == '"':
+                break
+            recipe_link += str(recipes_list[i])[link_iterator]
+            link_iterator += 1
+        link.append(recipe_link)
+
+        # Выбираем случайный рецепт из списка
+    random_choice = random.randint(0, len(recipes) - 1)
+    recipe_url = link[random_choice]
+    recipe_response = requests.get(recipe_url)
+    recipe_html_text = recipe_response.text
+    recipe_soup = BeautifulSoup(recipe_html_text, "html.parser")
+    recipe_title = recipes[random_choice]
+    recipe_ingredients = recipe_soup.find_all(class_='emotion-mdupit')
+    recipe_steps = recipe_soup.find_all(class_='emotion-wdt5in')
+    # Формируем текст рецепта
+    recipe_text = f'Рецепт для "{recipe_title}":\n\n'
+    recipe_text += 'Ингредиенты:\n'
+    for ingredient in recipe_ingredients:
+        recipe_text += f'- {ingredient.text.strip()}\n'
+    recipe_text += '\nПриготовление:\n'
+    for i, step in enumerate(recipe_steps):
+        recipe_text += f'{i+1}. {step.text.strip()}\n'
+
+    return recipe_text
+
 
 # Получаем HTML-код страницы с помощью метода get(url)
 search = str(input("Введите блюдо, которое вы хотите найти: "))
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from aiogram import Bot, types
 from aiogram.dispatcher.dispatcher import Dispatcher
+from aiogram.utils import executor
+
 import Parser
+
 token = "6077834064:AAEi70eoSxqqIu9YC3eiuQKB2jM1XjP9sMI"
 bot = Bot(token=token)
 dp = Dispatcher(bot)
@@ -9,15 +12,22 @@ url = "https://eda.ru/recepty"
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(msg: types.message):
-    await msg.reply_to_message(f'Привет, я - бот помощник повара. Приятно познакомиться, {msg.from_user.full_name}')
+    await msg.reply(f'Привет, я - бот помощник повара. Приятно познакомиться, {msg.from_user.full_name}')
 
 
-@dp.message_handler(content_types=['text'])
-async def get_text_messages(msg: types.Message):
-    if msg.text.lower() == 'привет':
-        await msg.answer('Привет!')
+@dp.message_handler(commands=['recipe'])  # обрабатываем команду /recipe
+async def get_recipe(msg: types.Message):
+    search = msg.text.split('/recipe ')[1]  # получаем запрос пользователя
+    recipe = Parser.get_recipe(search)  # вызываем функцию из Parser и получаем результат
+    if recipe:
+        await msg.answer(recipe)  # отправляем рецепт пользователю
     else:
-        await msg.answer('Не понимаю, что это значит.')
+        await msg.answer('Рецепт не найден')  # отправляем сообщение об ошибке
+
+
+@dp.message_handler(commands=['random'])
+async def handle_message(msg: types.Message):
+    await msg.answer(Parser.random_recipe())
 
 
 def documentation():
@@ -28,5 +38,5 @@ def documentation():
 
 
 if __name__ == '__main__':
-    # executor.start_polling(dp)
-    documentation()
+    executor.start_polling(dp)
+    # documentation()
